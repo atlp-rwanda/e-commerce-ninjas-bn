2b56e367893272fd7eecd1f25f1041e9f4c48630
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-05-14 17:55:55 +0200

    setting simple test

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,5 @@ app.use('/api', routes)
 app.use((req: Request, res: Response)=>{res.status(400).json({status: false, message: `Route ${req.method} ${req.url} not found.`})})
 
 app.listen(PORT, ()=>{console.log(`Server is running on port ${PORT}`)})
+
+export default app;
