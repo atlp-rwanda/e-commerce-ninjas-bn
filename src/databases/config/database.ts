a71f916dc5217fe2de4f163bc3ade24bfae35dd1
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-05-14 11:23:48 +0300

    Added sequelize

--- src/databases/config/database.ts
+++ src/databases/config/database.ts
@@ -1,1 +1,20 @@
-//Database connectivity
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  "***REMOVED***://code-***REMOVED***:test123@localhost:***REMOVED***/ecommerce"
+);
+
+export const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+  }
+};
+
+export const syncDatabase = async () => {
+  sequelize.sync({ force: true });
+};
+
+export default sequelize;
