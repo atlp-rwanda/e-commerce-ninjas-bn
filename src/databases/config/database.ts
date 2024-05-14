53c073ac475b0196a4dbce87f15c682f3db8a812
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-05-14 11:23:48 +0300

    Added sequelize

--- src/databases/config/database.ts
+++ src/databases/config/database.ts
@@ -1,1 +1,20 @@
-//Database connectivity
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  "postgres://code-ninjas:test123@localhost:5432/ecommerce"
+);
+
+export const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+  }
+};
+
+export const syncDatabase = async () => {
+  sequelize.sync({ force: true });
+};
+
+export default sequelize;
