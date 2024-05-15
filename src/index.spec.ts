e8d9258311122fa5bd64b460317bd5730dc6662d
ProgrammerDATCH <programmerdatch@gmail.com>
2024-05-15 19:20:57 +0200

    Added PORT Tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,12 +3,30 @@ import chaiHttp from 'chai-http';
 import app from './index';
 
 chai.use(chaiHttp);
+const router = () => chai.request(app)
 
-
-describe('GET /', () => {
+describe('Initial configuration', () => {
+    
+    // let server: any;
+    // before((done) => {
+    //     server = app.listen(0, () => {
+    //         done();
+    //     })
+    // });
+    // after((done)=>{
+    //     server.close(done);
+    // });
+    // it("Should use process.env.PORT if available", (done) => {
+    //     process.env.PORT = "5000";
+    //     server = app.listen(0, () => {
+    //         const port = server.address().port;
+    //         expect(port).to.equal(5000);
+    //         done();
+    //     });
+    // });
 
     it('should return 200', (done) => {
-        chai.request(app)
+        router()
             .get('/')
             .end((err, res) => {
                 expect(res).to.have.status(200);
