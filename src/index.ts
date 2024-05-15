7e279b761c42907062c0e3cc35f56524aba08102
ProgrammerDATCH <programmerdatch@gmail.com>
2024-05-15 17:46:51 +0200

    Modified structure

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import morgan from "morgan";
 import compression from "compression";
 import cors from "cors";
-import routes from "./routes";
 import SwaggerUi from "swagger-ui-express";
 import Document from "../swagger.json";
-import { syncDatabase, connectToDatabase } from "./databases/config/database";
 
-const PORT = process.env.PORT || 9090;
+dotenv.config();
 const app = express();
-
-connectToDatabase();
-syncDatabase();
+const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan(process.env.NODE_ENV));
 app.use(compression());
 app.use(cors());
 app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(Document));
@@ -25,13 +20,6 @@ app.get("/", (req: Request, res: Response) => {
   res
     .status(200)
     .json({ status: true, message: "Welcome to E-Commerce-Ninja-BackEnd" });
-});
-app.use("/api", routes);
-app.use((req: Request, res: Response) => {
-  res.status(400).json({
-    status: false,
-    message: `Route ${req.method} ${req.url} not found.`,
-  });
 });
 
 app.listen(PORT, () => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,1 @@
-import { Router } from "express";
-import { User } from "../databases/models/User";
-
-const routes = Router();
-
 // Use Routes
-
-routes.get('/add', async(req, res) => {
-    const randomNum = Math.floor(Math.random() * 1000);
-    const newUser = await User.create({names: `Emmy${randomNum}`, email: `emmy${randomNum}@gmail.com`, password: "P@ssword2024"})
-    res.status(201).json({status: true, message: "User registerd", user: newUser})
-});
-
-routes.get('/users', async(req, res) => {
-    res.status(201).json({status: true, message: await User.findAll() })
-});
-
-export default routes;
