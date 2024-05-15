0baeefdd770e56c815d61632c0b38df5aebe102c
Jadowacu1 <152473876+Jadowacu1@users.noreply.github.com>
2024-05-15 23:06:47 +0200

    Configure ESLint and Git Pre commit hook to run tests and ESlint format

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,20 +1,19 @@
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
-import app from './index';
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+import app from "./index";
 
 chai.use(chaiHttp);
-const router = () => chai.request(app)
+const router = () => chai.request(app);
 
-describe('Initial configuration', () => {
-
-    it('Should retutn `Welcome to E-Commerce-Ninja-BackEnd` when GET on /', (done) => {
-        router()
-            .get('/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.a('object');
-                expect(res.body).to.have.property('message', "Welcome to E-Commerce-Ninja-BackEnd");
-                done(err);
-            });
-    });
+describe("Initial configuration", () => {
+  it("Should retutn `Welcome to E-Commerce-Ninja-BackEnd` when GET on /", (done) => {
+    router()
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.property("message", "Welcome to E-Commerce-Ninja-BackEnd");
+        done(err);
+      });
+  });
 });
