1fb9ddac5025924273b36987354948c48a586a83
Mr. David <128073754+ProgrammerDATCH@users.noreply.github.com>
2024-05-15 20:25:47 +0200

    Modified structure of codebase to fit our needs (#6)
    * Modified structure of codebase to fit our needs
    
    * Modified backend structure to fit our needs
    
    * Modified structure
    
    * Modified structure
    
    * Added PORT Tests
    
    * New structure added

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import morgan from "morgan";
 import compression from "compression";
 import cors from "cors";
-import routes from "./routes";
 import SwaggerUi from "swagger-ui-express";
 import Document from "../swagger.json";
-import { syncDatabase, connectToDatabase } from "./databases/config/database";
 
-const PORT = process.env.PORT || 9090;
-const app = express();
+dotenv.config();
+const app: Express = express();
 
-connectToDatabase();
-syncDatabase();
+const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan(process.env.NODE_ENV));
 app.use(compression());
 app.use(cors());
-app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(Document));
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(Document));
 
 app.get("/", (req: Request, res: Response) => {
+  console.log("Index using PORT: ", PORT)
   res
     .status(200)
     .json({ status: true, message: "Welcome to E-Commerce-Ninja-BackEnd" });
-});
-app.use("/api", routes);
-app.use((req: Request, res: Response) => {
-  res.status(400).json({
-    status: false,
-    message: `Route ${req.method} ${req.url} not found.`,
-  });
 });
 
 app.listen(PORT, () => {
