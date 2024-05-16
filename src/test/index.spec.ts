682929f1b8007ba18151fba6cdc884a23b3b81fe
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-05-16 10:02:27 +0200

    setup unit testing

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -16,4 +16,43 @@ describe('GET /', () => {
                 done();
             });
     });
+
+    it('should return a welcome message', (done) => {
+        chai.request(app)
+           .get('/')
+           .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body.message).to.equal("Welcome to E-Commerce-Ninja-BackEnd");
+                done();
+            });
+    });
+    it('should return 400 for invalid HTTP method', (done) => {
+        chai.request(app)
+           .post('/') 
+           .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
+    it('should return 400 for invalid route', (done) => {
+        chai.request(app)
+           .get('/invalid-route')
+           .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+    }); 
+    
+    it('should respond within a reasonable time', (done) => {
+        const startTime = Date.now();
+        chai.request(app)
+           .get('/')
+           .end((err, res) => {
+                const duration = Date.now() - startTime;
+                expect(duration).to.be.lessThan(500); 
+                done();
+            });
+    });
+    
+    
 });
