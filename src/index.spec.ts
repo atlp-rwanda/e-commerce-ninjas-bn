b9c84f8721e1fb8360baaae47932a3fbfa63dcd6
Mr. David <128073754+ProgrammerDATCH@users.noreply.github.com>
2024-05-17 13:56:34 +0200

    Ch deploy application 187584908 (#11)
    * [starts #187584908] Testing deployment
    
    * [finishes #187584908] Completed testing deployment

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,13 +7,13 @@ const router = () => chai.request(app)
 
 describe('Initial configuration', () => {
 
-    it('Should retutn `Welcome to E-Commerce-Ninja-BackEnd` when GET on /', (done) => {
+    it('Should retutn `Welcome To e-Commerce Ninja BackEnd.` when GET on /', (done) => {
         router()
             .get('/')
             .end((err, res) => {
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.a('object');
-                expect(res.body).to.have.property('message', "Welcome to E-Commerce-Ninja-BackEnd");
+                expect(res.body).to.have.property('message', "Welcome To e-Commerce Ninja BackEnd.");
                 done(err);
             });
     });
