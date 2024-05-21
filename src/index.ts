7aea273ceb4b6db28223b2ac4c661e9a39d0a009
ProgrammerDATCH <programmerdatch@gmail.com>
2024-05-21 13:15:56 +0200

    Test auto deploy on render

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on the port ${PORT}`);
 });
 
 
