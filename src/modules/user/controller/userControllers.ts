4dd55eb1fe7d226dd9dda5cff6675d4171817c09
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-05-22 09:27:10 +0200

    started task

--- src/modules/user/controller/userControllers.ts
+++ src/modules/user/controller/userControllers.ts
@@ -1,1 +1,15 @@
 // user Controllers
+import { Request,Response } from "express";
+import userRepositories from "../repository/userRepositories";
+
+const registerUser = async (req:Request,res:Response) => {
+    try {
+        const email:string = req.params.email;
+        const userExists = await userRepositories.getUserByEmail(email);
+        if (userExists) {
+            
+        }
+    } catch (error) {
+        
+    }
+}
--- src/modules/user/repository/userRepositories.ts
+++ src/modules/user/repository/userRepositories.ts
@@ -1,1 +1,15 @@
 // user repositories
+import db from '../../../databases/models/index'
+const {Users} = db
+
+const registerUser = async (body:any) =>{
+    const user = await Users.create(body)
+    return user;
+}
+
+const getUserByEmail = async (email:string) =>{
+    const user = await Users.findOne({ where: { email: email} })
+    return user;
+}
+
+export default {registerUser, getUserByEmail}
