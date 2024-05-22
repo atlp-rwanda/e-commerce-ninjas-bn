2c826158332b8910832aa44345c0e760fd3f0945
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-05-22 11:47:57 +0300

    Ch setup sequelize 187584899 (#22)
    * Initial commit
    
    * Set up an empty Express Boilerplate with dotenv
    
    * Setup API documentation using swagger
    
    * Setting houndci badge
    
    * [Delivers #187584905] Integrate CirlceCI
    
    * Setting github action workflows
    
    * Setting github action workflows
    
    * Added sequelize
    
    * Added sequelize
    
    * Added Sequelize
    
    * Added sequelize
    
    * setting simple test
    
    * setting simple test
    
    * setting simple test
    
    * fixing spelling words
    
    * coverage status badge
    
    * setting codecov badge
    
    * Update ci.yml
    
    * Modified structure of codebase to fit our needs (#6)
    
    * Modified structure of codebase to fit our needs
    
    * Modified backend structure to fit our needs
    
    * Modified structure
    
    * Modified structure
    
    * Added PORT Tests
    
    * New structure added
    
    * [finishes #187612234] Update ReadMe to Check Pivotal Tracker (#10)
    
    * Setup docker (#9)
    
    * Setup docker
    
    Setup Docker image and hosted it on Docker Hub
    
    * [Finishes #187584909] Completed to setup docker
    
    * Ch deploy application 187584908 (#11)
    
    * [starts #187584908] Testing deployment
    
    * [finishes #187584908] Completed testing deployment
    
    * Test auto deployment (#14)
    
    * [finishes #187584862] Added EsLint and Pre-commit hook (#16)
    
    * [finishes #187584899] Configure The App to use PostgreSQL as DB Engine and Sequelize ORM
    
    * [fixes #187584899] wrong spelling in test
    
    * Added instructions in README file
    
    * Testing auto deployment (#18)
    
    * Testing auto deployment
    
    * Test auto deploy on render
    
    * Auto deploy only when Merged
    
    * Test auto deploy via hooks (#19)
    
    * Re-test auto deployment (#20)
    
    * [fixes #187584899] Added environment variable for testing(online) and production(online) even development
    
    * correct ci
    
    * [finishes #187584908] Auto Deploy app to render (#21)
    
    * secret variable in env.example
    
    * removed unwanted folder
    
    * modified testing script
    
    * modified ci
    
    * modified testing script
    
    * modified testing script
    
    * updating ci
    
    * updating ci
    
    * updating ci
    
    * [fixes #187584899] final
    
    * [fixes #187584899] final
    
    * [fixes #187584899] final
    
    * added .env.example
    
    * added .env.example
    
    * added .cache_ggshield into gitgnore
    
    * [fixes 187584899] gitguardian
    
    * [fixes 187584899] gitguardian
    
    ---------
    
    Co-authored-by: anguandia mike <mikeanguandia@gmail.com>
    Co-authored-by: ProgrammerDATCH <programmerdatch@gmail.com>
    Co-authored-by: Mr. David <128073754+ProgrammerDATCH@users.noreply.github.com>
    Co-authored-by: MANISHIMWESalton <149325279+MANISHIMWESalton@users.noreply.github.com>
    Co-authored-by: Saddock Kabandana <aimegetz@gmail.com>
    Co-authored-by: Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
    Co-authored-by: Fabrice-Dush <100077095+Fabrice-Dush@users.noreply.github.com>
    Co-authored-by: Ndahimana Bonheur <ndahimana154@gmail.com>

--- src/databases/models/index.ts
+++ src/databases/models/index.ts
@@ -1,1 +1,39 @@
-// Sequalize configuration with Database connection
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import fs from "fs";
+import path from "path";
+import { Sequelize, DataTypes } from "sequelize";
+import * as dbConnection from "../config/config"
+
+const db: any = {};
+let sequelize: Sequelize;
+const basename = path.basename(__filename);
+const env: string = process.env.NODE_ENV || "development";
+const config: any = dbConnection[env as keyof typeof dbConnection];
+
+if (config.url) {
+    sequelize = new Sequelize(config.url, config);
+} else {
+    sequelize = new Sequelize(config.database!, config.username!, config.password, config);
+}
+
+fs.readdirSync(__dirname)
+    .filter((file: string) => {
+        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts";
+    })
+    .forEach((file: string) => {
+        const modelPath = path.join(__dirname, file);
+        const model = require(modelPath)(sequelize, DataTypes);
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
