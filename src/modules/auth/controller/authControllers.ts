ff78044fe1e2f1918b88c1a79c6d27c4d0604f69
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-05-23 22:28:33 +0200

    [finishes 187584912] updated generateToken

--- src/modules/auth/controller/authControllers.ts
+++ src/modules/auth/controller/authControllers.ts
@@ -8,7 +8,7 @@ import { UsersAttributes } from "../../../databases/models/users";
 const registerUser = async (req: Request, res: Response): Promise<void> => {
         try {
                 const register:UsersAttributes = await userRepositories.registerUser(req.body);
-                const token: string = generateToken(register)
+                const token: string = generateToken(register.id);
                 res.status(httpStatus.OK).json({ user: register, token: token })
         }catch(error: any) {
                 res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: httpStatus.INTERNAL_SERVER_ERROR, error: error})
