ec6b830826e50ed02c9c200d1e723214caafc79a
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-05-24 19:41:34 +0200

    chore: add password- related functionality( forgot, reset, update)

--- src/modules/auth/repository/authRepositories.ts
+++ src/modules/auth/repository/authRepositories.ts
@@ -10,5 +10,27 @@ const findUserByEmail = async (email:string) =>{
     return await Users.findOne({ where: { email: email} })
 }
 
+const findUserById = async (userId: number) => {
+    return await Users.findOne({ where: { id: userId } });
+};
 
-export default {registerUser, findUserByEmail}
+const updateUserPassword = async (userId: number, newPassword: string) => {
+    return await Users.update({ password: newPassword }, { where: { id: userId } });
+};
+
+const updateUserLastPasswordChange = async (userId: number, lastPasswordChange: Date): Promise<void> => {
+    await Users.update({ lastPasswordChange: lastPasswordChange }, { where: { id: userId } });
+};
+
+
+const findUserByResetToken = async (resetPasswordToken: string) => {
+    return await Users.findOne({ where: { resetPasswordToken: resetPasswordToken } });
+};
+
+const updateUser = async (id: number, updateFields: any) => {
+    return await Users.update(updateFields, { where: { id: id } });
+};
+
+
+
+export default {registerUser, findUserByEmail, findUserById, updateUserPassword, updateUserLastPasswordChange , findUserByResetToken, updateUser }
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,15 @@
-import {validation,isAccountExist} from "../middlewares/validation";
+import { validation, isAccountExist } from "../middlewares/validation";
 import authControllers from "../modules/auth/controller/authControllers";
 import { Router } from "express";
-import {authSchema} from "../modules/auth/validation/authValidations";
+import { emailSchema ,updatePasswordSchema, resetPasswordSchema, authSchema,loginSchema } from "../modules/auth/validation/authValidations";
 
 const router: Router = Router();
 
 router.post("/register", validation(authSchema), isAccountExist, authControllers.registerUser);
+router.post("/login", validation(loginSchema), authControllers.login);
+router.post("/forgot-password", validation(emailSchema), authControllers.forgotPassword);
+router.get("/reset-password/:token", authControllers.verifyResetToken);
+router.post("/reset-password", validation(resetPasswordSchema), authControllers.resetPassword);
+router.post("/update-password", validation(updatePasswordSchema), authControllers.updatePassword);
 
 export default router;
