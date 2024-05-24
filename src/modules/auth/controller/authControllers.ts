414a8b3adfa5bb2286d723e9761ea150bfb429c5
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-05-24 19:41:34 +0200

    chore: add password- related functionality( forgot, reset, update)

--- src/modules/auth/controller/authControllers.ts
+++ src/modules/auth/controller/authControllers.ts
@@ -4,6 +4,11 @@ import userRepositories from "../repository/authRepositories";
 import { generateToken } from "../../../helpers";
 import httpStatus from "http-status";
 import { UsersAttributes } from "../../../databases/models/users";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import { sendForgotPasswordEmail } from "../../../services/sendEmail";
+import authRepositories from "../repository/authRepositories";
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req: Request, res: Response): Promise<void> => {
         try {
@@ -16,4 +21,142 @@ const registerUser = async (req: Request, res: Response): Promise<void> => {
         
 }
 
-export default { registerUser }
+
+const forgotPassword = async (req: Request, res: Response): Promise<void> => {
+        try {
+          
+    
+            const user: UsersAttributes | null = await authRepositories.findUserByEmail(req.body.email);
+    
+            if (!user) {
+                res.status(httpStatus.NOT_FOUND).json({ status: false, message: "User not found" });
+                return;
+            }
+    
+            const resetToken = crypto.randomBytes(8).toString("hex");
+            const resetPasswordExpires = new Date(Date.now() + 3600000); 
+    
+            await authRepositories.updateUser(user.id, {
+                resetPasswordToken: resetToken,
+                resetPasswordExpires: resetPasswordExpires
+            });
+    
+            await sendForgotPasswordEmail(user.email, resetToken, user.firstName); 
+    
+            res.status(httpStatus.OK).json({ status: true, message: "Password reset email sent" });
+        } catch (error: any) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: false, message: error.message });
+        }
+    };
+    
+    const verifyResetToken = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const { token } = req.params;
+            const user: UsersAttributes | null = await authRepositories.findUserByResetToken(token);
+    
+            if (!user || user.resetPasswordExpires! < new Date()) {
+                res.status(httpStatus.UNAUTHORIZED).json({ status: false, message: "Token is invalid or has expired" });
+                return;
+            }
+    
+            res.status(httpStatus.OK).json({ status: true, message: "Token is valid" });
+        } catch (error: any) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: false, message: error.message });
+        }
+    };
+    
+    const resetPassword = async (req: Request, res: Response): Promise<void> => {
+        try {
+       
+    
+            const { token, newPassword } = req.body;
+            const user: UsersAttributes | null = await authRepositories.findUserByResetToken(token);
+    
+            if (!user || user.resetPasswordExpires! < new Date()) {
+                res.status(httpStatus.UNAUTHORIZED).json({ status: false, message: "Token is invalid or has expired" });
+                return;
+            }
+    
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+    
+            await authRepositories.updateUser(user.id, {
+                password: hashedPassword,
+                resetPasswordToken: null,
+                resetPasswordExpires: null
+            });
+    
+            res.status(httpStatus.OK).json({ status: true, message: "Password has been reset" });
+        } catch (error: any) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: false, message: error.message });
+        }
+    };
+    
+    const updatePassword = async (req: Request, res: Response): Promise<void> => {
+        try {
+            
+          
+    
+            const { userId, oldPassword, newPassword } = req.body;
+    
+            const user = await authRepositories.findUserById(userId);
+    
+            if (!user) {
+                res.status(httpStatus.NOT_FOUND).json({ status: false, message: "User not found" });
+                return;
+            }
+            const isOldPasswordValid = await bcrypt.compare(oldPassword, user.password);
+            if (!isOldPasswordValid) {
+                res.status(httpStatus.UNAUTHORIZED).json({ status: false, message: "Old password is incorrect" });
+                return;
+            }
+    
+            const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    
+    
+            await authRepositories.updateUserPassword(userId, hashedNewPassword);
+    
+            res.status(httpStatus.OK).json({ status: true, message: "Password updated successfully" });
+        } catch (error: any) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: false, message: error.message });
+        }
+    };
+
+
+
+
+const login = async (req: Request, res: Response): Promise<void> => {
+    try {
+      
+        const { email, password } = req.body;
+        const user = await authRepositories.findUserByEmail(email);
+
+        if (!user) {
+            res.status(httpStatus.NOT_FOUND).json({ status: false, message: "User not found" });
+            return;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(httpStatus.UNAUTHORIZED).json({ status: false, message: "Wrong password" });
+            return;
+        }
+
+        const passwordExpirationTime = 30 * 24 * 60 * 60 * 1000;
+        const passwordLastChanged = user.lastPasswordChange || user.createdAt; 
+        const passwordExpired = new Date(passwordLastChanged.getTime() + passwordExpirationTime) < new Date();
+
+        if (passwordExpired) {
+            res.status(httpStatus.UNAUTHORIZED).json({ status: false, message: "Password expired, please change your password" });
+            return;
+        }
+
+
+        const token = jwt.sign({ userId: user.id }, "020d54824d694ac07ffe7779a843bbe3eb73a1a3bbf5d8f867b1ed3136b06396", { expiresIn: "1h" });
+
+        res.status(httpStatus.OK).json({ status: true, message: "Login successful", token: token });
+    } catch (error: any) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: false, message: error.message });
+    }
+};
+
+export default { registerUser,login, forgotPassword, verifyResetToken, resetPassword, updatePassword}
--- src/modules/auth/validation/authValidations.ts
+++ src/modules/auth/validation/authValidations.ts
@@ -14,7 +14,7 @@ interface User {
     role: "buyer" | "seller" | "admin";
 }
 
-const authSchema = Joi.object<User>({
+export const authSchema = Joi.object<User>({
     firstName: Joi.string().required().messages({
         "string.base": "First name should be a type of text",
         "string.empty": "First name cannot be an empty field",
@@ -73,4 +73,73 @@ const authSchema = Joi.object<User>({
     })
 });
 
-export {authSchema};
+export const loginSchema = Joi.object({
+    email: Joi.string().email().required().messages({
+        "string.base": "Email should be a type of text",
+        "string.email": "Email must be a valid email",
+        "string.empty": "Email cannot be an empty field",
+        "any.required": "Email is required"
+    }),
+    password: Joi.string().required().messages({
+        "string.base": "Password should be a type of text",
+        "string.empty": "Password cannot be an empty field",
+        "any.required": "Password is required"
+    })
+});
+
+
+export const emailSchema = Joi.object({
+    email: Joi.string().email().required().messages({
+        "string.base": "Email should be a type of text",
+        "string.email": "Email must be a valid email",
+        "string.empty": "Email cannot be an empty field",
+        "any.required": "Email is required"
+    })
+});
+
+export const resetPasswordSchema = Joi.object({
+    token: Joi.string().required().messages({
+        "any.required": "Token is required"
+    }),
+    newPassword: Joi.string().min(8).pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required().messages({
+        "string.base": "Password should be a type of text",
+        "string.empty": "Password cannot be an empty field",
+        "string.min": "Password should have a minimum length of 8",
+        "string.pattern.base": "Password must contain both letters and numbers",
+        "any.required": "Password is required"
+    }),
+    confirmPassword: Joi.any().valid(Joi.ref("newPassword")).required().messages({
+        "any.only": "Confirm password must match new password",
+        "any.required": "Confirm password is required"
+    })
+});
+
+
+export const updatePasswordSchema = Joi.object({
+    userId: Joi.number().required().messages({
+        "number.base": "User ID should be a number",
+        "any.required": "User ID is required"
+    }),
+    oldPassword: Joi.string().required().messages({
+        "string.base": "Old password should be a type of text",
+        "string.empty": "Old password cannot be an empty field",
+        "any.required": "Old password is required"
+    }),
+    newPassword: Joi.string().min(8).pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required().messages({
+        "string.base": "New password should be a type of text",
+        "string.empty": "New password cannot be an empty field",
+        "string.min": "New password should have a minimum length of 8",
+        "string.pattern.base": "New password must contain both letters and numbers",
+        "any.required": "New password is required"
+    }),
+    confirmPassword: Joi.any().valid(Joi.ref("newPassword")).required().messages({
+        "any.only": "Confirm password must match new password",
+        "any.required": "Confirm password is required"
+    })
+}).custom((value, helpers) => {
+    if (value.oldPassword === value.newPassword) {
+        return helpers.message({ custom: "Old password should not be similar to the new password" });
+    }
+    return value;
+});
+
