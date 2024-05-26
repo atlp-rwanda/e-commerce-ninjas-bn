ef841ad18bbad2aaaa85398877e43f404c292aa7
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-05-26 02:00:06 +0200

    [finishes #187584912] correcting test now it 100%

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,13 +1,9 @@
 import jwt from "jsonwebtoken"
-import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv"
+
 dotenv.config
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-});
+
  const generateToken = (id: number) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
   };
-  export { generateToken,cloudinary}
+  export { generateToken}
