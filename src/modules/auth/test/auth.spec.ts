d978ba52d26b91c2a7d43dc05895dd2db8aaaa43
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-05-26 22:06:23 +0200

    Added variables

--- src/modules/auth/test/auth.spec.ts
+++ src/modules/auth/test/auth.spec.ts
@@ -3,64 +3,28 @@ import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import sinon from "sinon";
 import httpStatus from "http-status";
-import fs from "fs"
-import path from "path";
 import app from "../../..";
 import { isUserExist } from "../../../middlewares/validation";
 import authRepositories from "../repository/authRepositories";
 import Users from "../../../databases/models/users";
-import * as uploadImages from "../../../helpers/uploadImage";
 
 
 
 chai.use(chaiHttp);
 const router = () => chai.request(app);
-
-const imagePath = path.join(__dirname, "../../../../public/ProjectManagement.jpg");
-const filePath = path.join(__dirname, "../../../../public/BUILD.txt");
-const fileBuffer = fs.readFileSync(filePath);
-const imageBuffer = fs.readFileSync(imagePath)
 describe("Authentication Test Cases", () => {
   it("Should be able to register new user", (done) => {
     router()
       .post("/api/auth/register")
-      .field("firstName", "TestUser")
-      .field("lastName", "TestUser")
-      .field("email", "TestUser@example.com")
-      .field("phone", 123456789)
-      .field("password", "TestUser@123")
-      .field("gender", "male")
-      .field("language", "english")
-      .field("currency", "USD")
-      .field("birthDate", "2000-12-12")
-      .field("role", "buyer")
-      .attach("profilePicture", imageBuffer, "ProjectManagement.jpg")
+      .send({
+        email: "user@example.com",
+        password: "userPassword@123"
+      })
       .end((error, response) => {
-        if (error || response.status !== 200) {
-          console.error("Error:", error);
-          console.log("Response Body:", response.body);
-        }
-        expect(response.status).to.equal(200);
+        expect(response.status).to.equal(httpStatus.OK);
         expect(response.body).to.be.a("object");
-        expect(response.body).to.have.property("user");
-        expect(response.body).to.have.property("token");
-        expect(response.body.user).to.have.property("firstName");
-        expect(response.body.user).to.have.property("lastName");
-        expect(response.body.user).to.have.property("email");
-        expect(response.body.user).to.have.property("phone");
-        expect(response.body.user).to.have.property("password");
-        expect(response.body.user).to.have.property("role");
-        expect(response.body.user).to.have.property("id");
-        expect(response.body.user).to.have.property("gender");
-        expect(response.body.user).to.have.property("language");
-        expect(response.body.user).to.have.property("currency");
-        expect(response.body.user).to.have.property("profilePicture");
-        expect(response.body.user).to.have.property("birthDate");
-        expect(response.body.user).to.have.property("status", true);
-        expect(response.body.user).to.have.property("isVerified", false);
-        expect(response.body.user).to.have.property("is2FAEnabled", false);
-        expect(response.body.user).to.have.property("createdAt");
-        expect(response.body.user).to.have.property("updatedAt");
+        expect(response.body).to.have.property("data");
+        expect(response.body.message).to.be.a("string");
         done(error);
       });
   });
@@ -68,17 +32,10 @@ describe("Authentication Test Cases", () => {
   it("should return validation return message error and 400", (done) => {
     router()
       .post("/api/auth/register")
-      .field("firstName", "TestUser")
-      .field("lastName", "TestUser")
-      .field("email", "TestUser@example.com")
-      .field("phone", "8888888")
-      .field("password", "TestUser")
-      .field("gender", "male")
-      .field("language", "english")
-      .field("currency", "USD")
-      .field("birthDate", "2000-12-12")
-      .field("role", "buyer")
-      .attach("profilePicture", imageBuffer, "ProjectManagement.jpg")
+      .send({
+        email: "user@example.com",
+        password: "userPassword"
+      })
       .end((error, response) => {
         expect(response.status).equal(400);
         expect(response.body).to.be.a("object");
@@ -86,27 +43,6 @@ describe("Authentication Test Cases", () => {
         done(error);
       });
   });
-})
-describe("multer test", () => {
-  it("should return fail", (done) => {
-    router()
-      .post("/api/auth/register")
-      .field("firstName", "TestUser")
-      .field("lastName", "TestUser")
-      .field("email", "TestUser@example.com")
-      .field("phone", "8888888")
-      .field("password", "TestUser")
-      .field("gender", "male")
-      .field("language", "english")
-      .field("currency", "USD")
-      .field("birthDate", "2000-12-12")
-      .field("role", "buyer")
-      .attach("profilePicture", fileBuffer, "BUILD.txt")
-      .end((err, res) => {
-        expect(res.body).to.be.an("object");
-        done(err);
-      });
-  })
 })
 
 describe("isUserExist Middleware", () => {
@@ -125,17 +61,10 @@ describe("isUserExist Middleware", () => {
 
     router()
       .post("/api/auth/register")
-      .field("firstName", "TestUser")
-      .field("lastName", "TestUser")
-      .field("email", "TestUser@example.com")
-      .field("phone", 123456789)
-      .field("password", "TestUser@123")
-      .field("gender", "male")
-      .field("language", "english")
-      .field("currency", "USD")
-      .field("birthDate", "2000-12-12")
-      .field("role", "buyer")
-      .attach("profilePicture", imageBuffer, "ProjectManagement.jpg")
+      .send({
+        email: "user@example.com",
+        password: "userPassword@123"
+      })
       .end((err, res) => {
         expect(res).to.have.status(httpStatus.BAD_REQUEST);
         expect(res.body).to.be.an("object");
@@ -149,12 +78,12 @@ describe("isUserExist Middleware", () => {
     sinon.stub(authRepositories, "findUserByEmail").throws(new Error("Database error"));
     router()
       .post("/auth/register")
-      .field("email", "usertesting@gmail.com")
+      .send({ email: "usertesting@gmail.com" })
       .end((err, res) => {
         expect(res).to.have.status(httpStatus.INTERNAL_SERVER_ERROR);
         expect(res.body).to.be.an("object");
         expect(res.body).to.have.property("status", httpStatus.INTERNAL_SERVER_ERROR);
-        expect(res.body).to.have.property("message","Database error");
+        expect(res.body).to.have.property("message", "Database error");
         done(err);
       });
   });
@@ -164,7 +93,7 @@ describe("isUserExist Middleware", () => {
 
     router()
       .post("/auth/register")
-      .field("email", "newuser@gmail.com")
+      .send({ email: "newuser@gmail.com" })
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body).to.be.an("object");
@@ -173,38 +102,28 @@ describe("isUserExist Middleware", () => {
       });
   });
 });
-
-describe("POST /api/auth/register error handling", () => {
-  let uploadImagesStub: sinon.SinonStub;
+describe("POST /auth/register - Error Handling", () => {
+  let registerUserStub: sinon.SinonStub;
 
   beforeEach(() => {
-      uploadImagesStub = sinon.stub(uploadImages, "uploadImages").throws(new Error("Invalid image file"));
+    registerUserStub = sinon.stub(authRepositories, "registerUser").throws(new Error("Test error"));
   });
 
   afterEach(() => {
-      uploadImagesStub.restore();
+    registerUserStub.restore();
   });
 
-  it("should return 500 and an error message when an error occurs", (done) => {
-      router()
-          .post("/api/auth/register")
-          .field("firstName", "TestUser")
-          .field("lastName", "TestUser")
-          .field("email", "TestUser@example.com")
-          .field("phone", 123456789)
-          .field("password", "TestUser@123")
-          .field("gender", "male")
-          .field("language", "english")
-          .field("currency", "USD")
-          .field("birthDate", "2000-12-12")
-          .field("role", "buyer")
-          .attach("profilePicture", Buffer.from("image content"), "ProjectManagement.jpg")
-          .end((err, res) => {
-              if (err) return done(err);
-              expect(res.status).to.equal(500);
-              expect(res.body).to.have.property("status", 500);
-              expect(res.body).to.have.property("message", "Invalid image file");
-              done();
-          });
+  it("should return 500 and error message when an error occurs", (done) => {
+    router()
+      .post("/api/auth/register")
+      .send({ email: "test@example.com", password: "password@123" })
+      .end((err, res) => {
+        expect(res.status).to.equal(httpStatus.INTERNAL_SERVER_ERROR);
+        expect(res.body).to.deep.equal({
+          status: httpStatus.INTERNAL_SERVER_ERROR,
+          message: "Test error"
+        });
+        done(err)
+      });
   });
 });
