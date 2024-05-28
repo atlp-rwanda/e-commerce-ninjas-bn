b41b548bf454e65cc8bf0b3a50ef40001bef6a20
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-05-28 10:30:56 +0300

    Added authorization middleware

--- src/modules/auth/repository/authRepositories.ts
+++ src/modules/auth/repository/authRepositories.ts
@@ -1,31 +1,52 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Users from "../../../databases/models/users"
-import Session from "../../../databases/models/session"
+import Users from "../../../databases/models/users";
+import Session from "../../../databases/models/session";
 
-const createUser = async (body:any) =>{
-    return await Users.create(body)
-}
+const createUser = async (body: any) => {
+  return await Users.create(body);
+};
 
+const findUserByAttributes = async (key: string, value: any) => {
+  return await Users.findOne({ where: { [key]: value } });
+};
 
-const findUserByAttributes = async (key:string, value:any) =>{
-    return await Users.findOne({ where: { [key]: value} })
-}
-
-const updateUserByAttributes = async (updatedKey:string, updatedValue:any, whereKey:string, whereValue:any) =>{
-    await Users.update({ [updatedKey]: updatedValue }, { where: { [whereKey]: whereValue} });
-    return await findUserByAttributes(whereKey, whereValue)
-}
+const updateUserByAttributes = async (
+  updatedKey: string,
+  updatedValue: any,
+  whereKey: string,
+  whereValue: any
+) => {
+  await Users.update(
+    { [updatedKey]: updatedValue },
+    { where: { [whereKey]: whereValue } }
+  );
+  return await findUserByAttributes(whereKey, whereValue);
+};
 
 const createSession = async (body: any) => {
-    return await Session.create(body);
+  return await Session.create(body);
+};
+
+const findSessionByUserId = async (userId: number) => {
+  return await Session.findOne({ where: { userId } });
+};
+
+const findSessionByUserIdAndToken = async (userId: number, token: string) => {
+  return await Session.findOne({where: {token, userId}})
 }
 
-const findSessionByUserId = async( userId:number ) => {
-    return await Session.findOne({ where: { userId } });
-}
 
-const destroySession = async (userId: number, token:string) =>{
-    return await Session.destroy({ where: {userId, token } });
-}
+const destroySession = async (userId: number, token: string) => {
+  return await Session.destroy({ where: { userId, token } });
+};
 
-export default { createUser, createSession, findUserByAttributes, destroySession, updateUserByAttributes, findSessionByUserId }
+export default {
+  createUser,
+  createSession,
+  findUserByAttributes,
+  destroySession,
+  updateUserByAttributes,
+  findSessionByUserId,
+  findSessionByUserIdAndToken
+};
