afc26583f6547c96f9c1d0fa17f19d924cb93ea3
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 00:56:27 +0200

    [delivers #187584923] Delivered with testing

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userControllers from "../modules/user/controller/userControllers";
-import {isUserExist, validation} from "../middlewares/validation";
+import {isUserExist, validation,validateUpdateUserRole} from "../middlewares/validation";
 import { statusSchema } from "../modules/user/validation/userValidations";
 
 
 const router: Router = Router()
 
 router.put("/admin-update-user-status/:id", validation(statusSchema), isUserExist, userControllers.updateUserStatus);
+
+router.put("/update-role/:id",validateUpdateUserRole, userControllers.updateUserRole);
 
 export default router;
