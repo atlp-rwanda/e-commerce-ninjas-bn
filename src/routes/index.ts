ec2ca0000fd34147473d1e3bf428a3aaca28c618
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 00:56:27 +0200

    [delivers #187584923] Delivered with testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express"
 import authRouter from "./authRouter"
+import userRouter from "./userRouter";
+
 
 const router: Router = Router()
 
 router.use("/auth", authRouter);
+router.use("/users", userRouter);
+
 
 export default router;
