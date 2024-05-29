46227b53a05d5eaff02ee43bec7fcb4f2157f8ae
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 11:42:56 +0200

    mend

--- src/modules/user/controller/userControllers.ts
+++ src/modules/user/controller/userControllers.ts
@@ -1,21 +1,21 @@
 // user Controllers
 import { Request, Response } from "express";
 
-// Import User repository
 import authRepositories from "../../auth/repository/authRepositories";
+import httpStatus from "http-status";
 
 const updateUserRole = async (req: Request, res: Response) => {
   try {
-    await authRepositories.UpdateUserByAttributes("role",req.body.role,"id",req.params.id)
-    const updatedUser = await authRepositories.findUserByAttributes("id",req.params.id)
-    return res.status(200).json({
-      success: true,
+    await authRepositories.UpdateUserByAttributes("role", req.body.role, "id", req.params.id)
+    const updatedUser = await authRepositories.findUserByAttributes("id", req.params.id)
+    return res.status(httpStatus.OK).json({
+      status: httpStatus.OK,
       message: "User role updated successfully",
       new: updatedUser
     });
   } catch (error) {
-    return res.status(500).json({
-      success: false,
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: httpStatus.INTERNAL_SERVER_ERROR,
       message: "An error occurred while updating the user role."
     });
   }
