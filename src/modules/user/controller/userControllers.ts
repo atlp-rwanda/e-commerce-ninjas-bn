478b8016993dfc614139f9cd9cb2f0b496e946e1
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-30 00:30:49 +0200

    mend

--- src/modules/user/controller/userControllers.ts
+++ src/modules/user/controller/userControllers.ts
@@ -22,10 +22,6 @@ const updateUserRole = async (req: Request, res: Response) => {
 };
 
 
-export default { updateUserRole };import { Request, Response } from "express";
-import httpStatus from "http-status";
-import authRepositories from "../../auth/repository/authRepositories";
-
 const updateUserStatus = async (req: Request, res: Response): Promise<void> => {
   try {
     const userId: number = Number(req.params.id);
@@ -36,22 +32,6 @@ const updateUserStatus = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-const updateUserRole = async (req: Request, res: Response) => {
-  try {
-    await authRepositories.UpdateUserByAttributes("role", req.body.role, "id", req.params.id)
-    const updatedUser = await authRepositories.findUserByAttributes("id", req.params.id)
-    return res.status(httpStatus.OK).json({
-      status: httpStatus.OK,
-      message: "User role updated successfully",
-      new: updatedUser
-    });
-  } catch (error) {
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      status: httpStatus.INTERNAL_SERVER_ERROR,
-      message: error
-    });
-  }
-};
 
 
-export default { updateUserStatus,updateUserRole };// user Controllers
+export default { updateUserStatus,updateUserRole };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,27 +1,12 @@
-
-import express from "express";
-
-// Import controller
-import userControllers from "../modules/user/controller/userControllers";
-
-// Import Validations
-import {updateUserRoleSchema, validateUpdateUserRole, validation} from "../middlewares/validation"
-
-const userRouter = express.Router();
-
-// Update the users role
-userRouter.put("/admin-update-role/:id",validation(updateUserRoleSchema),validateUpdateUserRole, userControllers.updateUserRole);
-
-export default userRouter;
 import { Router } from "express";
 import userControllers from "../modules/user/controller/userControllers";
-import {isUserExist, validation,validateUpdateUserRole} from "../middlewares/validation";
+import {isUserExist, validation,validateUpdateUserRole,updateUserRoleSchema} from "../middlewares/validation";
 import { statusSchema } from "../modules/user/validation/userValidations";
+
 
 const router: Router = Router()
 
 router.put("/admin-update-user-status/:id", validation(statusSchema), isUserExist, userControllers.updateUserStatus);
-
-router.put("/update-role/:id",validateUpdateUserRole, userControllers.updateUserRole);
+router.put("/admin-update-role/:id",validation(updateUserRoleSchema),validateUpdateUserRole, userControllers.updateUserRole);
 
 export default router;
