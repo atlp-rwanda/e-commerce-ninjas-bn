52bcf131cc4a280d488be8c58cd7af485a1b01dd
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 11:07:29 +0200

    mend

--- src/modules/user/controller/userControllers.ts
+++ src/modules/user/controller/userControllers.ts
@@ -2,21 +2,18 @@
 import { Request, Response } from "express";
 
 // Import User repository
-import userRepositories from "../repository/userRepositories";
+import authRepositories from "../../auth/repository/authRepositories";
 
 const updateUserRole = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const role = req.body.role;
   try {
-    await userRepositories.updateUserRoleFx(Number(id), String(role));
-    const updatedUser = await userRepositories.getSingleUserFx(Number(id));
+    await authRepositories.UpdateUserByAttributes("role",req.body.role,"id",req.params.id)
+    const updatedUser = await authRepositories.findUserByAttributes("id",req.params.id)
     return res.status(200).json({
       success: true,
       message: "User role updated successfully",
       new: updatedUser
     });
   } catch (error) {
-    console.error(error);
     return res.status(500).json({
       success: false,
       message: "An error occurred while updating the user role."
