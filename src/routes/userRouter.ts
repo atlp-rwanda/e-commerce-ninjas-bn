326b4cc0899b269d1b6e7924c68767b6e97af415
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 17:46:27 +0200

    mend

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,7 +3,6 @@ import userControllers from "../modules/user/controller/userControllers";
 import {isUserExist, validation,validateUpdateUserRole} from "../middlewares/validation";
 import { statusSchema } from "../modules/user/validation/userValidations";
 
-
 const router: Router = Router()
 
 router.put("/admin-update-user-status/:id", validation(statusSchema), isUserExist, userControllers.updateUserStatus);
