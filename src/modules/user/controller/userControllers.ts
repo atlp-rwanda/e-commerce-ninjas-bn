a9d5ff5e24a49e49def7650f2b2476a9f4044fa3
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 10:04:35 +0200

    [finished #187584923] Feature admin should update user role

--- src/modules/user/controller/userControllers.ts
+++ src/modules/user/controller/userControllers.ts
@@ -11,4 +11,21 @@ const updateUserStatus = async (req: Request, res: Response): Promise<void> => {
     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ status: httpStatus.INTERNAL_SERVER_ERROR, message: error.message });
   }
 };
-export default { updateUserStatus };
+
+const updateUserRole = async (req: Request, res: Response) => {
+  try {
+    await authRepositories.UpdateUserByAttributes("role", req.body.role, "id", req.params.id)
+    const updatedUser = await authRepositories.findUserByAttributes("id", req.params.id)
+    return res.status(httpStatus.OK).json({
+      status: httpStatus.OK,
+      message: "User role updated successfully",
+      new: updatedUser
+    });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: httpStatus.INTERNAL_SERVER_ERROR,
+      message: error
+    });
+  }
+};
+export default { updateUserStatus,updateUserRole };
