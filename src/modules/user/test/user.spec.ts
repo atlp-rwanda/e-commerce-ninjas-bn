720537585a91e8fec1b3853e486aa8791d1ee7f1
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-29 10:04:35 +0200

    [finished #187584923] Feature admin should update user role

--- src/modules/user/test/user.spec.ts
+++ src/modules/user/test/user.spec.ts
@@ -1,24 +1,20 @@
-// user Tests
 import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import httpStatus from "http-status";
-
 import app from "../../..";
 
 chai.use(chaiHttp);
 
 const router = () => chai.request(app);
 
-// let userId: number;
-// let userRole: string;
-// let testUser: number = 1;
-const testRole: string = "Buyer";
-describe("Admin - Changing user roles", () => {
+let userId: number;
+
+describe("Admin update User roles", () => {
   it("should register a new user", (done) => {
     router()
       .post("/api/auth/register")
       .send({
-        email: "ndahimana123@gmail.com",
+        email: "bonheurndahimana125@gmail.com",
         password: "userPassword@123"
       })
       .end((error, response) => {
@@ -26,76 +22,55 @@ describe("Admin - Changing user roles", () => {
         expect(response.body).to.be.an("object");
         expect(response.body).to.have.property("data");
         userId = response.body.data.id;
-        userRole = response.body.data.role;
-        // console.log("New user id",userId)
         expect(response.body).to.have.property("message", "Account created successfully. Please check email to verify account.");
         done(error);
       });
   });
 
-  // it("Should update user role and return new user", (done) => {
-  //   // console.log("Out of box New user id",userId)
-  //   router()
-  //     .put(`/api/users/update-role/${userId}`)
-  //     .send({
-  //       role: `${testRole}`
-  //     })
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(200);
-  //       expect(res.body).to.be.a("object");
-  //       expect(res.body).to.have.property("success", true);
-  //       expect(res.body).to.have.property(
-  //         "message",
-  //         "User role updated successfully"
-  //       );
-  //       done(err);
-  //     });
-  // });
-
-  // it("Should notify when no new role provided", (done) => {
-  //   router()
-  //     .put(`/api/users/update-role/${userId}`)
-  //     .send({})
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(400);
-  //       expect(res.body).to.be.a("object");
-  //       expect(res.body).to.have.property("success", false);
-  //       expect(res.body).to.have.property(
-  //         "message",
-  //         "The 'role' parameter is required."
-  //       )
-  //       done(err);
-  //     });
-  // });
-
-  it("SHould notify if the user is not found", (done) => {
+  it("Should notify if no role is specified", (done) => {
     router()
-      .put("/api/users/update-role/100000")
-      .send({
-        role: `${testRole}`
-      })
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.body).to.be.a("object");
-        expect(res.body).to.have.property("success", false);
-        expect(res.body).to.have.property("message", "User does't exist.");
-        done(err);
+      .put(`/api/users/update-role/${userId}`)
+      .end((error, response) => {
+        expect(response.status).to.equal(400);
+        expect(response.body).to.have.property("success", false);
+        done(error);
       });
   });
 
-  // it("Should throw an error when Invalid ID is passed", (done) => {
-  //   router()
-  //     .put("/api/users/update-role/invalid_id")
-  //     .send({ role: `${testRole}` })
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(500);
-  //       expect(res.body).to.be.a("object");
-  //       expect(res.body).to.have.property("success", false);
-  //       expect(res.body).to.have.property(
-  //         "message",
-  //         "An error occurred while updating the user role."
-  //       );
-  //       done(err);
-  //     });
-  // });
+  it("Should notify if the role is other than ['Admin', 'Buyer', 'Seller']", (done) => {
+    router().put(`/api/users/update-role/${userId}`).send({ role: "Hello" }).end((error, response) => {
+      expect(response.status).to.equal(400);
+      expect(response.body).to.have.property("success", false);
+      expect(response.body).to.have.property("message", "The 'role' parameter must be one of ['Admin', 'Buyer', 'Seller'].");
+      done(error);
+    });
+  })
+
+  it("Should notify if the user is not found", (done)=>{
+    router().put(`/api/users/update-role/1039482383218223289321242545`).send({ role: "Admin" }).end((error, response) => {
+      expect(response.status).to.equal(404);
+      expect(response.body).to.have.property("success", false);
+      expect(response.body).to.have.property("message", "User doesn't exist.");
+      done(error);
+    });
+  })
+
+
+  // it("Should notify if the invalid id is sent to server", (done)=>{
+  //   router().put(`/api/users/update-role/invalid_id`).send({ role: "Admin" }).end((error, response) => {
+  //     expect(response.status).to.equal(500);
+  //     expect(response.body).to.have.property("success", false);
+  //     expect(response.body).to.have.property("message", "An error occurred while updating the user role.");
+  //     done(error);
+  //   });
+  // })
+
+  it("Should notify if the role is updated successfully", (done)=>{
+    router().put(`/api/users/update-role/${userId}`).send({ role: "Admin" }).end((error, response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property("success", true);
+      expect(response.body).to.have.property("message", "User role updated successfully");
+      done(error);
+    });
+  })
 });
