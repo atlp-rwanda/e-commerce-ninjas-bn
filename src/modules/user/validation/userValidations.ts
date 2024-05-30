06ccbd461a679107323ce8826f9e04f9ec35dd6c
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-30 14:29:39 +0200

    Feature Admin update role 187584923 (#36)
    * [delivers #187584923] Delivered with testing
    
    * [finished #187584923] Feature admin should update user role
    
    * [finishes #187584923] Feature admin should update user role
    
    * mend
    
    * [finishes #187584923] Feature admin should update user role 1
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * [delivers #187584915] Added user login (#35)
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * [delivers #187584923] Delivered with testing
    
    * [finished #187584923] Feature admin should update user role
    
    * mend
    
    * [finishes #187584923] Feature admin should update user role 1
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * [Delivers #187584922] Admin should be able to disable an account (#25)
    
    * [Delivers #187584922] Admin should be able to disable an account
    
    * Admin should be able to disable or enable an account
    
    * Admin should be able to disable or enable an account
    
    * Admin should be able to disable or enable an account
    
    * Admin should be able to disable or enable an account
    
    * Admin should be able to disable or enable an account
    
    * Adjusted the database
    
    * DB adjusted
    
    * adjusted info
    
    * done rebase
    
    * mend
    
    * mend
    
    * [delivers #187584923] Delivered with testing
    
    * [finished #187584923] Feature admin should update user role
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * [delivers #187584923] Delivered with testing
    
    * [finished #187584923] Feature admin should update user role
    
    * mend
    
    * [finishes #187584923] Feature admin should update user role 1
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * [finishes #187584923] finishes fix the errors
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    ---------
    
    Co-authored-by: Mr. David <128073754+ProgrammerDATCH@users.noreply.github.com>
    Co-authored-by: Saddock Kabandana <aimegetz@gmail.com>

--- src/modules/user/validation/userValidations.ts
+++ src/modules/user/validation/userValidations.ts
@@ -7,3 +7,12 @@ export const statusSchema = Joi.object({
     "any.required": "Status is required"
   })
 });
+
+
+export const roleSchema = Joi.object({
+  role: Joi.string().valid("Admin", "Buyer", "Seller").required().messages({
+      "any.required": "The 'role' parameter is required.",
+      "string.base": "The 'role' parameter must be a string.",
+      "any.only": "Only Admin, Buyer and Seller are allowed."
+  })
+});
