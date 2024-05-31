64aa4865842ce41f94995b94fe1b68962fcb3f2b
Solange Duhimbaze Ihirwe <159579750+solangeihirwe03@users.noreply.github.com>
2024-05-31 11:58:43 +0200

    Logout feature (#26)
    * [#187584914]added logout feature
    
    * [starts #187584914] added logout feature
    
    * [finishes#187584914] logout feature
    
    * [delivers##187584914] updated readme & swagger.json
    
    * [delivers##187584914] updated readme & swagger.json
    
    * [deliveres #187584914] logout features completed
    
    * [deliveres #187584914] logout features completed
    
    * [delivers #187584914] finished logout feature

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,15 +1,46 @@
 import { Router } from "express";
 import authControllers from "../modules/auth/controller/authControllers";
-import { validation, isUserExist, isAccountVerified, verifyUserCredentials } from "../middlewares/validation";
-import { emailSchema, credentialSchema } from "../modules/auth/validation/authValidations";
-
+import {
+  validation,
+  isUserExist,
+  isAccountVerified,
+  verifyUserCredentials
+} from "../middlewares/validation";
+import {
+  emailSchema,
+  credentialSchema
+} from "../modules/auth/validation/authValidations";
+import { userAuthorization } from "../middlewares/authorization";
 
 const router: Router = Router();
 
-router.post("/register", validation(credentialSchema), isUserExist, authControllers.registerUser);
-router.get("/verify-email/:token", isAccountVerified, authControllers.verifyEmail);
-router.post("/send-verify-email", validation(emailSchema), isAccountVerified, authControllers.sendVerifyEmail);
-router.post("/login", validation(credentialSchema), verifyUserCredentials, authControllers.loginUser);
-
+router.post(
+  "/register",
+  validation(credentialSchema),
+  isUserExist,
+  authControllers.registerUser
+);
+router.get(
+  "/verify-email/:token",
+  isAccountVerified,
+  authControllers.verifyEmail
+);
+router.post(
+  "/send-verify-email",
+  validation(emailSchema),
+  isAccountVerified,
+  authControllers.sendVerifyEmail
+);
+router.post(
+  "/login",
+  validation(credentialSchema),
+  verifyUserCredentials,
+  authControllers.loginUser
+);
+router.post(
+  "/logout",
+  userAuthorization(["buyer", "seller", "admin"]),
+  authControllers.logoutUser
+);
 
 export default router;
