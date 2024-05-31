ebcd7db4284f495f05530a64cce6ba8a865734cb
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-05-31 09:53:13 +0200

    ft: add user-update-password route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import userControllers from "../modules/user/controller/userControllers";
 import {isUserExist, validation} from "../middlewares/validation";
-import { statusSchema,roleSchema } from "../modules/user/validation/userValidations";
+import { statusSchema,roleSchema, updatePasswordSchema } from "../modules/user/validation/userValidations";
 
 
 const router: Router = Router()
 
 router.put("/admin-update-user-status/:id", validation(statusSchema), isUserExist, userControllers.updateUserStatus);
 router.put("/admin-update-role/:id",validation(roleSchema),isUserExist, userControllers.updateUserRole);
+router.put("/user-update-password/:id", validation(updatePasswordSchema), isUserExist, userControllers.updateUserPassword);
 
 export default router;
