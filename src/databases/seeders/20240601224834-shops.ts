12991e1dea0168f1cc17f76cfc64d74a13e6ae04
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-06-07 04:16:53 +0200

    [delivers #187584928] Delivers the Seller and User list products

--- src/databases/seeders/20240601224834-shops.ts
+++ src/databases/seeders/20240601224834-shops.ts
@@ -2,18 +2,26 @@ import { QueryInterface } from "sequelize";
 import { shopOneId, userThreeId } from "../../types/uuid";
 
 export const up = async (queryInterface: QueryInterface) => {
-    await queryInterface.bulkInsert("shops", [
-        {
-            id: shopOneId,
-            name: "Shoes",
-            userId: userThreeId,
-            description: "Shoes",
-            createdAt: new Date(),
-            updatedAt: new Date()
+    const existingShop = await queryInterface.rawSelect("shops", {
+        where: {
+            id: shopOneId
         }
-    ]);
+    }, ["id"]);
+
+    if (!existingShop) {
+        await queryInterface.bulkInsert("shops", [
+            {
+                id: shopOneId,
+                name: "Shoes",
+                userId: userThreeId,
+                description: "Shoes",
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ]);
+    }
 };
 
 export const down = async (queryInterface: QueryInterface) => {
-    await queryInterface.bulkDelete("shops", null, {});
+    await queryInterface.bulkDelete("shops", { id: shopOneId }, {});
 };
