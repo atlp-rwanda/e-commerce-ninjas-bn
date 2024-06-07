5dad8761b14c0b810d1a8e7c4d29e041a89680cc
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-06-07 14:57:27 +0200

    [Delivers #ft-seller-and-buyer-list-items-187584928] Tests version 1

--- src/modules/auth/test/auth.spec.ts
+++ src/modules/auth/test/auth.spec.ts
@@ -164,7 +164,7 @@ describe("Authentication Test Cases", () => {
       });
   });
 
-
+  
   it("Should be able to login a registered user", (done) => {
     router()
       .post("/api/auth/login")
@@ -196,7 +196,7 @@ describe("Authentication Test Cases", () => {
         done(err);
       });
   });
-
+  
   it("should return internal server error on login", (done) => {
     sinon
       .stub(authRepositories, "createSession")
@@ -572,7 +572,7 @@ describe("Passport Configuration", () => {
 
 describe("Google Authentication Strategy", () => {
   it("should call the strategy callback with correct parameters", () => {
-  });
+     });
 });
 
 function googleAuthenticationCallback(
@@ -635,71 +635,69 @@ describe("Google Authentication Strategy Callback", () => {
 });
 
 describe("Google Authentication", () => {
-  describe("Google Strategy", () => {
-    it("should call the done callback with user object", () => {
-      const requestMock: Partial<Request> = {};
-      const accessTokenMock = "mockAccessToken";
-      const refreshTokenMock = "mockRefreshToken";
-      const profileMock = {
-        id: "mockUserId",
-        emails: [{ value: "test@example.com" }],
-        name: { givenName: "John", familyName: "Doe" },
-        photos: [{ value: "https://example.com/profile.jpg" }]
-      };
-      const doneStub = sinon.stub();
+    describe("Google Strategy", () => {
+      it("should call the done callback with user object", () => {
+        const requestMock: Partial<Request> = {};
+        const accessTokenMock = "mockAccessToken";
+        const refreshTokenMock = "mockRefreshToken";
+        const profileMock = {
+          id: "mockUserId",
+          emails: [{ value: "test@example.com" }],
+          name: { givenName: "John", familyName: "Doe" },
+          photos: [{ value: "https://example.com/profile.jpg" }]
+        };
+        const doneStub = sinon.stub();
 
+      
+        googleAuth.passport._strategies.google._verify(
+          requestMock as Request,
+          accessTokenMock,
+          refreshTokenMock,
+          profileMock,
+          doneStub
+        );
 
-      googleAuth.passport._strategies.google._verify(
-        requestMock as Request,
-        accessTokenMock,
-        refreshTokenMock,
-        profileMock,
-        doneStub
-      );
-
-      sinon.assert.calledWith(doneStub, null, {
-        userId: "mockUserId",
-        email: "test@example.com",
-        firstName: "John",
-        lastName: "Doe",
-        picture: "https://example.com/profile.jpg",
-        accToken: "mockAccessToken"
+        sinon.assert.calledWith(doneStub, null, {
+          userId: "mockUserId",
+          email: "test@example.com",
+          firstName: "John",
+          lastName: "Doe",
+          picture: "https://example.com/profile.jpg",
+          accToken: "mockAccessToken"
+        });
       });
     });
   });
-});
 
-describe("authenticateViaGoogle", () => {
-  let req: Partial<Request>;
-  let res: Partial<Response>;
-  let next: NextFunction;
-  let resJsonSpy: sinon.SinonSpy;
-  let resStatusSpy: sinon.SinonStub;
-
-  beforeEach(() => {
-    req = {};
-    res = {
-      json: sinon.spy(),
-      status: sinon.stub().returnsThis()
-    };
-    next = sinon.spy() as NextFunction;
-    resJsonSpy = res.json as sinon.SinonSpy;
-    resStatusSpy = res.status as sinon.SinonStub;
-  });
-
-  it("should respond with 401 if authentication fails", async () => {
-    const authenticateStub = sinon.stub(passport, "authenticate").callsFake((strategy, callback) => {
-      callback(null, null);
-      return (req: Request, res: Response) => { };
+  describe("authenticateViaGoogle", () => {
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    let next: NextFunction;
+    let resJsonSpy: sinon.SinonSpy;
+    let resStatusSpy: sinon.SinonStub;
+  
+    beforeEach(() => {
+      req = {};
+      res = {
+        json: sinon.spy(),
+        status: sinon.stub().returnsThis()
+      };
+      next = sinon.spy() as NextFunction;
+      resJsonSpy = res.json as sinon.SinonSpy;
+      resStatusSpy = res.status as sinon.SinonStub;
     });
-
-    await googleAuth.authenticateWithGoogle(req as Request, res as Response, next);
-
-    expect(resStatusSpy.calledWith(401)).to.be.true;
-    expect(resJsonSpy.calledWith({ error: "Authentication failed" })).to.be.true;
-
-    authenticateStub.restore();
+  
+    it("should respond with 401 if authentication fails", async () => {
+      const authenticateStub = sinon.stub(passport, "authenticate").callsFake((strategy, callback) => {
+        callback(null, null); 
+        return (req: Request, res: Response) => { };
+      });
+  
+      await googleAuth.authenticateWithGoogle(req as Request, res as Response, next);
+  
+      expect(resStatusSpy.calledWith(401)).to.be.true;
+      expect(resJsonSpy.calledWith({ error: "Authentication failed" })).to.be.true;
+  
+      authenticateStub.restore();
+    });
   });
-});
-
-
