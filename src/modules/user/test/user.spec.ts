4839f45c21e832a06ac5ea41335c990126b2ce73
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-06-08 08:32:29 +0200

    [Delivers #187584928] Handle gitguardian issues.

--- src/modules/user/test/user.spec.ts
+++ src/modules/user/test/user.spec.ts
@@ -30,7 +30,7 @@ describe("Update User Status test case ", () => {
       .post("/api/auth/register")
       .send({
         email: "nda1234@gmail.com",
-        password: "userPassword@123"
+        password: "_REMOVED123"
       })
       .end((error, response) => {
         expect(response.status).to.equal(httpStatus.CREATED);
@@ -194,7 +194,7 @@ describe("Admin update User roles", () => {
       .post("/api/auth/register")
       .send({
         email: "nda12345@gmail.com",
-        password: "_REMOVED123"
+        password: "$321!Pass!123$"
       })
       .end((error, response) => {
         expect(response.status).to.equal(httpStatus.CREATED);
