e040802d6ac1a59ad8c5e89d7c3d86ba988a68b1
Saddock Kabandana <aimegetz@gmail.com>
2024-06-13 20:37:05 +0200

    [Delivers #187584943] ft-notifications

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -61,7 +61,7 @@ router.put(
   userAuthorization(["seller"]),
   upload.array("images"),
   validation(productUpdateSchema),
-  isProductExist,
+  isProductExistById,
   productController.sellerUpdateProduct
 );
 
