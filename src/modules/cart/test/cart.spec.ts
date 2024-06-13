204771b85456dc02ab24d92e923111ebcae22781
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-06-13 16:05:31 +0200

    mend

--- src/modules/cart/test/cart.spec.ts
+++ src/modules/cart/test/cart.spec.ts
@@ -9,9 +9,30 @@ chai.use(chaiHttp);
 const router = () => chai.request(app);
 
 describe("Buyer Test cases", () => {
-  let buyerWithCartToken: string;
-  let buyerWithoutCartToken: string;
-  before((done) => {
+
+  it("Should return 'No cart' when buyer have no cart.", (done) => {
+    router()
+      .post("/api/auth/login")
+      .send({
+        email: "buyer2@gmail.com",
+        password: "Password@123"
+      })
+      .end((error, response) => {
+        if (error) done(error);
+        const buyerWithoutCartToken = response.body.data.token;
+        router().get("/api/cart/buyer-get-cart")
+          .set("Authorization", `Bearer ${buyerWithoutCartToken}`)
+          .end((err, res) => {
+            if (err) done(err);
+            expect(res).to.have.status(httpStatus.NOT_FOUND);
+            expect(res.body).to.be.an("object");
+            expect(res.body).to.have.property("message").that.is.a("string");
+            done();
+          });
+      });
+  });
+
+  it("Should return Cart details when buyer have pending cart.", (done) => {
     router()
       .post("/api/auth/login")
       .send({
@@ -20,41 +41,18 @@ describe("Buyer Test cases", () => {
       })
       .end((error, response) => {
         if (error) done(error);
-        buyerWithCartToken = response.body.data.token;
-        router()
-          .post("/api/auth/login")
-          .send({
-            email: "buyer2@gmail.com",
-            password: "Password@123"
-          })
+        const buyerWithCartToken = response.body.data.token;
+        router().get("/api/cart/buyer-get-cart")
+          .set("Authorization", `Bearer ${buyerWithCartToken}`)
           .end((err, res) => {
             if (err) done(err);
-            buyerWithoutCartToken = res.body.data.token;
+            expect(res).to.have.status(httpStatus.OK);
+            expect(res.body).to.be.an("object");
+            expect(res.body).to.have.property("message").that.is.a("string");
+            expect(res.body).to.have.property("data").that.is.an("object");
             done();
-          })
-      })
-  })
-  it("Should return 'No cart' when buyer have no cart.", (done) => {
-    router().get("/api/cart/buyer-get-cart")
-      .set("Authorization", `Bearer ${buyerWithoutCartToken}`)
-      .end((err, response) => {
-        if (err) done(err);
-        expect(response).to.have.status(httpStatus.NOT_FOUND);
-        expect(response.body).to.be.an("object");
-        expect(response.body).to.have.property("message").that.is.a("string");
-        done()
-      })
-  })
-  it("Should return Cart details when buyer have pending cart.", (done) => {
-    router().get("/api/cart/buyer-get-cart")
-      .set("Authorization", `Bearer ${buyerWithCartToken}`)
-      .end((err, response) => {
-        if (err) done(err);
-        expect(response).to.have.status(httpStatus.OK);
-        expect(response.body).to.be.an("object");
-        expect(response.body).to.have.property("message").that.is.a("string");
-        expect(response.body).to.have.property("data").that.is.an("object");
-        done()
-      })
-  })
-})
+          });
+      });
+  });
+
+});
