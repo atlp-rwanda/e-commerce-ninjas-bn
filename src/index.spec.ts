f34fbcc57d1641b59b9c4198bf1615849d345b5b
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-06-14 14:58:56 +0200

    update notifications

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -318,8 +318,9 @@ describe("checkPasswordExpiration middleware", () => {
       setHeader: sinon.stub(),
     };
     next = sinon.spy();
+    sinon.stub(sendEmail, "sendEmail").resolves();
   });
-
+  
   afterEach(() => {
     sinon.restore();
   });
@@ -344,6 +345,11 @@ describe("checkPasswordExpiration middleware", () => {
       message: "Password expired, please check your email to reset your password.",
     });
     expect(next).to.not.have.been.called;
+    expect(sendEmail).to.have.been.calledWith(
+      "user@example.com",
+      "Password Expired - Reset Required",
+      sinon.match.string
+    );
   });
 
   it("should set header if the password is expiring soon", async () => {
