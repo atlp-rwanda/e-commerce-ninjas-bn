bb06e4bf6830202196e2878b29e2516dc39e6177
Saddock Kabandana <aimegetz@gmail.com>
2024-07-08 06:55:35 +0200

    fx-product-seeders (#88)

--- src/databases/migrations/20240601223524-create-products.ts
+++ src/databases/migrations/20240601223524-create-products.ts
@@ -25,7 +25,7 @@ export default {
         type: DataTypes.STRING,
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: true,
       },
       price: {
--- src/types/uuid.ts
+++ src/types/uuid.ts
@@ -18,6 +18,11 @@ export const shopTwoId = uuidv4();
 export const productOneId = uuidv4();
 export const productTwoId = uuidv4();
 export const productThreeId = uuidv4();
+export const productFourId = uuidv4();
+export const productFiveId = uuidv4();
+export const productSixId = uuidv4();
+export const productSevenId = uuidv4();
+export const productEightId = uuidv4();
 
 export const orderOneId = uuidv4();
 export const orderTwoId = uuidv4();
