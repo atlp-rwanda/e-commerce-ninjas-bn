83121a255b32bc89677cd5d27f7b690c171de533
hbapte <ijbapte@gmail.com>
2024-08-13 10:16:56 +0200

    ft admin set and update password expiry time

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import userControllers from "../modules/user/controller/userControllers";
 import { isUserExist, validation, isUsersExist, credential, isNotificationsExist, isUserProfileComplete, isSellerRequestExist } from "../middlewares/validation";
 import { userAuthorization } from "../middlewares/authorization";
-import { statusSchema, roleSchema, userSchema, changePasswordSchema, changeAddressSchema } from "../modules/user/validation/userValidations";
+import { statusSchema, roleSchema, userSchema, changePasswordSchema, changeAddressSchema, passwordExpirationTimeSchema } from "../modules/user/validation/userValidations";
 import upload from "../helpers/multer";
 
   const router = Router();
@@ -11,11 +11,13 @@ import upload from "../helpers/multer";
   router.get("/admin-get-user/:id", userAuthorization(["admin"]), isUserExist, userControllers.adminGetUser);
   router.put("/admin-update-user-status/:id", userAuthorization(["admin"]), validation(statusSchema), isUserExist, userControllers.updateUserStatus);
   router.put("/admin-update-user-role/:id", userAuthorization(["admin"]), validation(roleSchema), isUserExist, userControllers.updateUserRole);
-
+  router.put("/admin-update-password-expiration", userAuthorization(["admin"]), validation(passwordExpirationTimeSchema), userControllers.updatePasswordExpirationSetting);
+  router.get("/admin-get-password-expiration", userAuthorization(["admin"]), userControllers.getPasswordExpiration);
 
   router.get("/user-get-profile", userAuthorization(["admin", "buyer", "seller"]), userControllers.getUserDetails);
   router.put("/user-update-profile", userAuthorization(["admin", "buyer", "seller"]), upload.single("profilePicture"), validation(userSchema), userControllers.updateUserProfile);
   router.put("/change-password", userAuthorization(["admin", "buyer", "seller"]), validation(changePasswordSchema), credential, userControllers.changePassword);
+
 
 router.get("/user-get-notifications", userAuthorization(["admin", "buyer", "seller"]), isNotificationsExist, userControllers.getAllNotifications);
 router.get("/user-get-notification/:id", userAuthorization(["admin", "buyer", "seller"]),isNotificationsExist, userControllers.getSingleNotification);
